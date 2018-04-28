d74f4f2fb3be822371eea64f552e84ce7803d369
Erik L <loide.erik@gmail.com>
2018-04-28 10:25:38 +0900

    add a python file

---
+++ hello.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyderエディタ
+
+これは一時的なスクリプトファイルです
+"""
+
+print('hello world')
+print('I like 6.00.1x!')
+
+a = 0 
+b = 1
+
+if a < b:
+    print ("True")
+else:
+    print ("false")
