76db010a006e3d2da1d7f8a30d93c032c267a071
Erik L <loide.erik@gmail.com>
2018-05-15 09:51:36 +0900

    Dictionary notes

---
+++ dicts.py
@@ -0,0 +1,25 @@
+"""
+Functions are first class objects.
+They have types, can be elements of data structures and can appear in expressions.
+We can use functions as arguments in lists, which will give us the power of
+higher order programming.
+
+Now we can do a lot of things.
+
+seq[i] - ith element of sequence
+len[seq] - length of sequence
+seq1 + seq2 - concatenation of sequence
+n * seq - sequence that repeats seq n times 
+seq[start:end] - slice of sequence
+e in seq - true if e contained in sequence
+e not in seq - true if e contained in sequence
+for e in seq - iterates over elements of sequence.
+
+DICTIONARIES
+have keys and values.
+
+my_dict = {}
+grades = ('ana':'b', "john":'b')
+
+"""
+
